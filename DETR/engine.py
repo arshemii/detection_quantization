fe8aa0bd3b2b268d207aea6e86e30962e8b30310
arshemii <arshemii1373.2@gmail.com>
2024-12-23 15:38:55 +0100

    Update engine.py

--- DETR/engine.py
+++ DETR/engine.py
@@ -12,9 +12,7 @@ import torch
 import util.misc as utils
 from datasets.coco_eval import CocoEvaluator
 from datasets.panoptic_eval import PanopticEvaluator
-import openvino as ov
-from torchvision.transforms import ToTensor
-import torch.nn.functional as F
+
 
 def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
                     data_loader: Iterable, optimizer: torch.optim.Optimizer,
@@ -155,130 +153,6 @@ def evaluate(model, criterion, postprocessors, data_loader, base_ds, device, out
         stats['PQ_all'] = panoptic_res["All"]
         stats['PQ_th'] = panoptic_res["Things"]
         stats['PQ_st'] = panoptic_res["Stuff"]
-    return stats, coco_evaluator, samples, outputs
-
-def out_proc(res_ov):
-   
-    res_ov_log = torch.from_numpy(res_ov(0).data)
-    res_ov_box = torch.from_numpy(res_ov(1).data)
-
-    res_ov_dict = {
-        'pred_logits': res_ov_log,
-        'pred_boxes': res_ov_box}
-    
-    return res_ov_dict
+    return stats, coco_evaluator
 
 
-def sam_prec(samples):
-    #transform = ToTensor()
-    #samples = transform(samples)
-    #samples = samples.unsqueeze(0)
-    samples = F.interpolate(samples, size=(600, 800), mode='bilinear', align_corners=False)
-    samples = samples.numpy()
-    samples = samples[:, :, :, ::-1]
-    array_min = samples.min()  # Find the minimum value in the array
-    array_max = samples.max()  # Find the maximum value in the array
-
-    # Rescale the array to the range [0, 1]
-    samples = (samples - array_min) / (array_max - array_min)
-    samples = samples*255
-    
-    
-    return samples
-
-@torch.no_grad()
-def evaluate_ov(model, criterion, postprocessors, data_loader, base_ds, device, output_dir, input_layer):
-    #model.eval()
-    a_infer = model.create_infer_request()
-    criterion.eval()
-
-    metric_logger = utils.MetricLogger(delimiter="  ")
-    metric_logger.add_meter('class_error', utils.SmoothedValue(window_size=1, fmt='{value:.2f}'))
-    header = 'Test:'
-
-    iou_types = tuple(k for k in ('segm', 'bbox') if k in postprocessors.keys())
-    coco_evaluator = CocoEvaluator(base_ds, iou_types)
-    # coco_evaluator.coco_eval[iou_types[0]].params.iouThrs = [0, 0.1, 0.5, 0.75]
-
-    panoptic_evaluator = None
-    if 'panoptic' in postprocessors.keys():
-        panoptic_evaluator = PanopticEvaluator(
-            data_loader.dataset.ann_file,
-            data_loader.dataset.ann_folder,
-            output_dir=os.path.join(output_dir, "panoptic_eval"),
-        )
-    m = 0
-    for samples, targets in metric_logger.log_every(data_loader, 10, header):
-        m =m+1
-        print(m)
-        if m == 50:
-            break
-        #samples = samples.to(device)
-        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
-
-        img = sam_prec(samples.tensors)
-        a_infer.set_tensor(input_layer, ov.Tensor(img))
-        a_infer.start_async()
-        res_ov = a_infer.get_output_tensor
-        a_infer.wait()
-        outputs = out_proc(res_ov)
-        loss_dict = criterion(outputs, targets)
-        weight_dict = criterion.weight_dict
-
-        # reduce losses over all GPUs for logging purposes
-        loss_dict_reduced = utils.reduce_dict(loss_dict)
-        loss_dict_reduced_scaled = {k: v * weight_dict[k]
-                                    for k, v in loss_dict_reduced.items() if k in weight_dict}
-        loss_dict_reduced_unscaled = {f'{k}_unscaled': v
-                                      for k, v in loss_dict_reduced.items()}
-        metric_logger.update(loss=sum(loss_dict_reduced_scaled.values()),
-                             **loss_dict_reduced_scaled,
-                             **loss_dict_reduced_unscaled)
-        metric_logger.update(class_error=loss_dict_reduced['class_error'])
-
-        orig_target_sizes = torch.stack([t["orig_size"] for t in targets], dim=0)
-        results = postprocessors['bbox'](outputs, orig_target_sizes)
-        if 'segm' in postprocessors.keys():
-            target_sizes = torch.stack([t["size"] for t in targets], dim=0)
-            results = postprocessors['segm'](results, outputs, orig_target_sizes, target_sizes)
-        res = {target['image_id'].item(): output for target, output in zip(targets, results)}
-        if coco_evaluator is not None:
-            coco_evaluator.update(res)
-
-        if panoptic_evaluator is not None:
-            res_pano = postprocessors["panoptic"](outputs, target_sizes, orig_target_sizes)
-            for i, target in enumerate(targets):
-                image_id = target["image_id"].item()
-                file_name = f"{image_id:012d}.png"
-                res_pano[i]["image_id"] = image_id
-                res_pano[i]["file_name"] = file_name
-
-            panoptic_evaluator.update(res_pano)
-        
-        
-    # gather the stats from all processes
-    metric_logger.synchronize_between_processes()
-    print("Averaged stats:", metric_logger)
-    if coco_evaluator is not None:
-        coco_evaluator.synchronize_between_processes()
-    if panoptic_evaluator is not None:
-        panoptic_evaluator.synchronize_between_processes()
-
-    # accumulate predictions from all images
-    if coco_evaluator is not None:
-        coco_evaluator.accumulate()
-        coco_evaluator.summarize()
-    panoptic_res = None
-    if panoptic_evaluator is not None:
-        panoptic_res = panoptic_evaluator.summarize()
-    stats = {k: meter.global_avg for k, meter in metric_logger.meters.items()}
-    if coco_evaluator is not None:
-        if 'bbox' in postprocessors.keys():
-            stats['coco_eval_bbox'] = coco_evaluator.coco_eval['bbox'].stats.tolist()
-        if 'segm' in postprocessors.keys():
-            stats['coco_eval_masks'] = coco_evaluator.coco_eval['segm'].stats.tolist()
-    if panoptic_res is not None:
-        stats['PQ_all'] = panoptic_res["All"]
-        stats['PQ_th'] = panoptic_res["Things"]
-        stats['PQ_st'] = panoptic_res["Stuff"]
-    return stats, coco_evaluator, outputs, samples, img
